4b6e3b28d07473b904cc2e8cf39aa6b7a38fc9a8
sidd-sharma22 <siddharthsharma2219@gmail.com>
2026-05-26 21:12:47 +0530

    Added status codes in user and task routes

--- Project-3/app/routes.py
+++ Project-3/app/routes.py
@@ -10,13 +10,13 @@ router = APIRouter()
 # User Routes
 
 ## Create a user
-@router.post("/users", response_model=schemas.UserResponse, status_code=201)
+@router.post("/users", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = crud.create_user(db, user)
 
     if not new_user:
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already exists"
         )
 
@@ -24,19 +24,19 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 ## Get all user
-@router.get("/users", response_model=list[schemas.UserResponse])
+@router.get("/users", response_model=list[schemas.UserResponse], status_code=status.HTTP_200_OK)
 def get_users(db: Session = Depends(get_db)):
     return crud.get_users(db)
 
 
 ## Get a user
-@router.get("/users/{user_id}", response_model=schemas.UserResponse)
+@router.get("/users/{user_id}", response_model=schemas.UserResponse, status_code=status.HTTP_200_OK)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = crud.get_user(db, user_id)
 
     if not user:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found"
         )
 
@@ -44,13 +44,13 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 
 
 ## Get all tasks assigned to user
-@router.get("/users/{user_id}/tasks", response_model=list[schemas.TaskResponse])
+@router.get("/users/{user_id}/tasks", response_model=list[schemas.TaskResponse], status_code=status.HTTP_200_OK)
 def get_tasks_by_user(user_id: int, db: Session = Depends(get_db)):
     user = crud.get_user(db, user_id)
 
     if not user:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found"
         )
 
@@ -58,13 +58,13 @@ def get_tasks_by_user(user_id: int, db: Session = Depends(get_db)):
 
 
 ## Get all users assigned with task
-@router.get("/users/{user_id}/full", response_model=schemas.UserWithTasks)
+@router.get("/users/{user_id}/full", response_model=schemas.UserWithTasks, status_code=status.HTTP_200_OK)
 def get_user_with_tasks(user_id: int, db: Session = Depends(get_db)):
     user = crud.get_user_with_tasks(db, user_id)
 
     if not user:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found"
         )
 
@@ -74,13 +74,13 @@ def get_user_with_tasks(user_id: int, db: Session = Depends(get_db)):
 # Task Routes
 
 ## Create a Task
-@router.post("/tasks", response_model=schemas.TaskResponse, status_code=201)
+@router.post("/tasks", response_model=schemas.TaskResponse, status_code=status.HTTP_201_CREATED)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     new_task = crud.create_task(db, task)
 
     if not new_task:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found"
         )
 
@@ -89,19 +89,19 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
 
 
 ## Get All Tasks
-@router.get("/tasks", response_model=list[schemas.TaskResponse], status_code=200)
+@router.get("/tasks", response_model=list[schemas.TaskResponse], status_code=status.HTTP_200_OK)
 def get_tasks(db: Session = Depends(get_db)):
     return crud.get_tasks(db)
 
 
 ## Get One Task
-@router.get("/tasks/{task_id}", response_model=schemas.TaskResponse, status_code=200)
+@router.get("/tasks/{task_id}", response_model=schemas.TaskResponse, status_code=status.HTTP_200_OK)
 def get_task(task_id: int, db: Session = Depends(get_db)):
     task = crud.get_task(db, task_id)
 
     if not task:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Task not found"
         )
 
@@ -109,13 +109,13 @@ def get_task(task_id: int, db: Session = Depends(get_db)):
 
 
 ## Update Task
-@router.put("/tasks/{task_id}", response_model=schemas.TaskResponse, status_code=202)
+@router.put("/tasks/{task_id}", response_model=schemas.TaskResponse, status_code=status.HTTP_202_ACCEPTED)
 def update_task(task_id: int, task: schemas.TaskCreate, db: Session = Depends(get_db)):
     updated_task = crud.update_task(db, task_id, task)
 
     if not updated_task:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Task not found"
         )
 
@@ -123,13 +123,13 @@ def update_task(task_id: int, task: schemas.TaskCreate, db: Session = Depends(ge
 
 
 ## Delete Task
-@router.delete("/tasks/{task_id}", status_code=204)
+@router.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
     deleted_task = crud.delete_task(db, task_id)
 
     if not deleted_task:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Task not found"
         )
 
